4acdd0832b20eaa09fcfbc783a3d5e28961bf25b
KEKACHY <kekachy@mail.ru>
2025-03-25 02:29:27 +0300

    Backend

--- app/config.py
+++ app/config.py
@@ -3,15 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TEST_DATABASE_URI = os.getenv("SQLALCHEMY_TEST_DATABASE_URI")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+    SECRET_KEY = os.environ["SECRET_KEY"]
 
-# Строка подключения к БД PostgreSQL
-SQLALCHEMY_DATABASE_URL = Config.SQLALCHEMY_DATABASE_URI
-
-# Создаем подключение к базе данных
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# Создаем сессию для работы с БД
+# Инициализация подключения
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
