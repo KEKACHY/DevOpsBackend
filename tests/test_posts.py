441b280b9740abdfeeddead6ce6bed4271ea4e41
KEKACHY <kekachy@mail.ru>
2025-03-25 04:50:15 +0300

    Backend

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app
-from config import Config
+from .main import app
+from .config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
